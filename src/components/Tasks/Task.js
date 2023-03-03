95640621b165e7f1be32c5ff42c8400f1768d8f7
Sofiane Khenoune <modestman1986@yahoo.com>
2023-03-03 17:32:59 +0100

    Sub-components done

---
+++ src/components/Tasks/Task.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+function Task({text}) {
+  // Condition si tache faite (isDone ? 'list-item' : '.list-item--done')}
+  return (
+    <li>
+      <label className="list-item">
+        <input type="checkbox"/>
+        {text}
+      </label>
+    </li>
+  );
+}
+
+Task.prototype= {
+  text : PropTypes.string.isRequired,
+}
+
+export default Task;
--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,26 +1,10 @@
 import './styles.scss';
+import Task from './Task';
 
 function Tasks() {
   return (
     <ul class="list">
-      <li>
-        <label class="list-item list-item--done">
-          <input type="checkbox" checked/>
-          Ketchup
-        </label>
-      </li>
-      <li>
-        <label class="list-item">
-          <input type="checkbox"/>
-          Pâtes
-        </label>
-      </li>
-      <li>
-        <label class="list-item">
-          <input type="checkbox"/>
-          Salade
-        </label>
-      </li>
+      <Task text='Ketchup'/>
     </ul>
   );
 }
