f610590d13380efed4033aed42f0a2d3f97c3fb7
Sofiane Khenoune <modestman1986@yahoo.com>
2023-03-03 16:56:05 +0100

    Code in host Slow VM

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,10 @@
-import reactLogo from './react-logo.svg';
+
 import './styles.css';
 
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
-      <h1>Composant : App</h1>
+      
     </div>
   );
 }
