5e13aebf9cd40adcda813a006582a0485ddec7e1
Sofiane <modestman1986@yahoo.com>
2023-06-20 17:49:42 +0200

    Install React icons and add delete button in task component

--- src/components/Tasks/Task.js
+++ src/components/Tasks/Task.js
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
+import { MdDeleteForever } from "react-icons/md";
 
-function Task({ label, done, id, setTaskState,removeTaskInState }) {
+function Task({ label, done, id, setTaskState, removeTaskInState }) {
   // Condition si tache faite (isDone ? 'list-item' : '.list-item--done')}
   return (
     <li className="li-item">
@@ -19,11 +20,18 @@ function Task({ label, done, id, setTaskState,removeTaskInState }) {
         />
         {label}
       </label>
-      <span onClick={() => {
-        removeTaskInState(id);
-      }}>
-        X
-      </span>
+      <MdDeleteForever
+        color="#b53c3c"
+        size={40}
+        style={{
+          backgroundColor: "#fff",
+          borderRadius: "0.5em",
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          removeTaskInState(id);
+        }}
+      />
     </li>
   );
 }
@@ -33,7 +41,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   label: PropTypes.string.isRequired,
   removeTaskInState: PropTypes.func.isRequired,
-  setTaskState: PropTypes.func.isRequired
-}
+  setTaskState: PropTypes.func.isRequired,
+};
 
 export default React.memo(Task);
